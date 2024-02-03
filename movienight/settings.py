ec9ac7b51783dcd59e9918549bed37179bb0e453
Jeehyun Yoon (Peter) <dbswlgus0203@gmail.com>
2024-02-02 21:28:10 -0500

    update settings.py

--- movienight/settings.py
+++ movienight/settings.py
@@ -16,6 +16,8 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+DJANGO_ENV = os.environ.get('DJANGO_ENV', 'deveopment')
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
@@ -80,13 +82,24 @@ WSGI_APPLICATION = "movienight.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+if DJANGO_ENV =='develoopment':
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
     }
-}
+else:
+    DATABASES = {
+        'default' : {
+            "ENGINE" : 'django.db.backends.postgresql.psycopg2',
+            "NAME" : os.environ.get('DB_NAME'),
+            "USER" : os.environ.get('DB_USER'),
+            "PASSWORD" : os.environ.get('DB_PASSWORD'),
+            "HOST" : os.environ.get('DB_HOST'),
+            "PORT" : os.environ.get('DB_PORT', '5432'),
+        }
+    }
 
 
 # Password validation
